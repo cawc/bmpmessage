ab7000372e2cd1b18096408286aede8837db8b35
Robin <robinlievens24@gmail.com>
2018-07-20 19:34:45 +0200

    argparse, add 'secure' mode, debug prints removed

--- bmpmessage.py
+++ bmpmessage.py
@@ -23,18 +23,16 @@ noncompressed = {
 	11
 }
 
-def process_bmp(filename, message):
+def process_bmp(filename, message, mode):
 	with open(filename, 'rb+') as file:
 		data = file.read()
 		fileheader = data[:14]
 		headerfield = fileheader[0:2].decode('ascii')
-		#print(f'Headerfield: {headerfield}')
 
 		if headerfield not in headerfields:
 			raise(RuntimeError('BMP format not supported'))
 
 		#filesize = struct.unpack('<i',fileheader[2:6])[0]
-		#print(f'Size: {filesize} bytes')
 
 		pixelarray_offset = struct.unpack('<i',fileheader[10:14])[0]
 
@@ -45,22 +43,13 @@ def process_bmp(filename, message):
 		height = struct.unpack('<i',dibheader[8:12])[0]
 		bitsperpixel = struct.unpack('<h',dibheader[14:16])[0]
 
-		#print(f'Width: {width} pixels')
-		#print(f'Height: {height} pixels')
-		#print(f'Color depth: {bitsperpixel} bits')
-
 		compressionmethod = struct.unpack('<i', dibheader[16:20])[0]
-		#print(f'Compression method: {compressionmethods[compressionmethod]}')
 		if compressionmethod not in noncompressed:
 			raise(RuntimeError('BMP is compressed, not supported'))
 
 		#numberofcolors = struct.unpack('<I',dibheader[32:36])[0]
 
-		#print(f'Pixelarray offset: {pixelarray_offset}')
-
 		pixelarray_size = int(width*height*bitsperpixel/8)
-
-		#print(f'Pixelarray size: {pixelarray_size} bytes')
 
 		if len(message) > pixelarray_size:
 			raise RuntimeError('Image too small for message')
@@ -68,6 +57,8 @@ def process_bmp(filename, message):
 		print('Generating key...')
 		key = genkey(len(message), pixelarray_offset, pixelarray_offset+pixelarray_size)
 		print('Writing message...')
+		if mode == 'es':
+			randomiseimage(pixelarray_offset, pixelarray_offset+pixelarray_size, file)
 		writemessage(key, message, file)
 		key = '#'.join(map(str,key))
 		print(f'Message written. Key = {key}')
@@ -87,6 +78,11 @@ def writemessage(key, message, file):
 			file.seek(key[i])
 			file.write(message[i].encode('ascii'))
 
+def randomiseimage(minoff, maxoff, file):
+	for i in range(minoff, maxoff):
+		file.seek(i)
+		file.write(randint(0,255).to_bytes(1,'little'))
+
 def readmessage(filename, key):
 	with open(filename, 'rb') as file:
 		result = ''
@@ -96,18 +92,20 @@ def readmessage(filename, key):
 		print(result)
 
 def main():
-	if len(sys.argv) < 4 or sys.argv[1] not in ('e','d'):
-		print('Usage: bmpmessage.py e(ncode) [filename] [message]\n       bmpmessage.py d(ecode) [filename] [key]')
-		return
-	mode = sys.argv[1]
-	filename = sys.argv[2]
+	parser = argparse.ArgumentParser()
+	parser.add_argument('mode', choices=['e','d','es'], help='(e)ncode\n(d)ecode\n(es)ecure (overwrites image)')
+	parser.add_argument('filename', help='name of file/path to file')
+	parser.add_argument('mk', nargs='+', help='message to encode or key to decode, depending on mode option')
+	args = parser.parse_args()
 
-	if mode == 'e':
-		message = ' '.join(sys.argv[3:])
-		process_bmp(filename, message)
-	else:
-		key = map(int,sys.argv[3].split('#'))
-		readmessage(filename,key)
+
+
+	if args.mode == 'e' or args.mode == 'es':
+		message = ' '.join(args.mk)
+		process_bmp(args.filename, message, args.mode)
+	elif args.mode == 'd':
+		key = map(int,args.mk[0].split('#'))
+		readmessage(args.filename, key)
 
 if __name__ == '__main__':
 	main()
